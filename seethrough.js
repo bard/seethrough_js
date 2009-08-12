4b32f3077282357186064486e986d38a2cdc44dc
Massimiliano Mirra <bard@hyperstruct.net>
2009-08-12 12:16:01 +0200

    Empty attribute would cause node to be added with appendChild, which Rhino didn't like at all.

--- seethrough.js
+++ seethrough.js
@@ -276,9 +276,10 @@ seethrough.compile.element = function(xmlElement) {
                 var xmlChild;
                 for(var i=0,l=xmlChildren.length(); i<l; i++) {
                     xmlChild = xmlChildren[i];
-                    if(xmlChild.nodeKind() == 'attribute' &&
-                       xmlChild.toString() != '')
-                        xmlOut.@[xmlChild.name()] = xmlChild.toString();
+                    if(xmlChild.nodeKind() == 'attribute') {
+                        if(xmlChild.toString() != '')
+                            xmlOut.@[xmlChild.name()] = xmlChild.toString();
+                    }
                     else
                         xmlOut.appendChild(xmlChild);
                 }
